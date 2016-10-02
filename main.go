5ab589785eee582ec54b9a5a2272a9945701d3ef
devincarr <devin.carr0@gmail.com>
2016-10-01 19:37:14 -0500

    basic

--- main.go
+++ main.go
@@ -1,18 +1,27 @@
 package main
 
 import (
+	"github.com/devincarr/timequeue"
 	"io"
 	"net/http"
-	//	"github.com/devincarr/timequeue"
 )
+
+var tq = timequeue.NewTimeQueue()
+
+var count = 0
 
 func push(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "Hello World!")
+	tq.Push(0.1, 123, "http://getmuted.com/db/check")
+}
+
+func check(w http.ResponseWriter, r *http.Request) {
+	count += 1
+	io.WriteString(w, string(count))
 }
 
 func main() {
-	//	tq := timequeue.NewTimeQueue()
-	http.HandleFunc("/", push)
+	http.HandleFunc("/check", check)
 	http.HandleFunc("/push", push)
 	http.ListenAndServe(":8090", nil)
 }
